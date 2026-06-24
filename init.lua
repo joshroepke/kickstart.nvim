52b7c48a09c9bdad429d7a9b04234b3ad02e5f85
Josh Roepke <josh.roepke@mlse.com>
2026-06-24 13:38:36 -0400

    fix/update terraform lsp

--- init.lua
+++ init.lua
@@ -622,7 +622,8 @@ require('lazy').setup({
           -- This may be unwanted, since they displace some of your code
           if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
             map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+              local enabled = vim.lsp.inlay_hint.is_enabled { bufnr = event.buf }
+              vim.lsp.inlay_hint.enable(not enabled, { bufnr = event.buf })
             end, '[T]oggle Inlay [H]ints')
           end
         end,
